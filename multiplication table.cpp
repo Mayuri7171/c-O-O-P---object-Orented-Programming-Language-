ec2b4f3feeb4751f70fd470071ddd417249a337c
Mayuri Balasaheb Lagad7171 <156415794+Mayuri7171@users.noreply.github.com>
2024-07-14 17:33:16 +0530

    Add files via upload

---
+++ multiplication table.cpp
@@ -0,0 +1,18 @@
+// multiplication table
+
+using namespace std;
+#include<iostream>
+int main()
+{
+	int n;
+	
+	cout<<"enter number";
+	cin>> n;
+	
+	cout<<"Table="<<endl;
+	
+	for (int i=1;i<=10;i++)
+	{
+		cout<<n*i<<endl;
+	}
+}
