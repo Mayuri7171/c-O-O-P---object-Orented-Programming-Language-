46835d1af15fd6ff4d68582f1153bc9e6ac1b2bd
Mayuri Balasaheb Lagad7171 <156415794+Mayuri7171@users.noreply.github.com>
2024-07-14 17:32:57 +0530

    Add files via upload

---
+++ Accept name rno percentage and display using c++.cpp
@@ -0,0 +1,40 @@
+//Accept name rno percentage and display using c++
+
+using namespace std;
+#include<iostream>
+
+class  student
+
+{
+	public:
+		
+		int rno;
+		char name[50];
+		float per;
+		void accept()
+		
+		
+		{
+			cout<<"Enter the roll no name per:";
+			cin>>rno>>name>>per;
+		}
+		
+		void disp()
+		{
+			cout<<"roll no="<<rno<<endl;
+			cout<<"name="<<name<<endl;
+			cout<<"percentage="<<per<<endl;
+		}
+		
+};
+
+
+int main()
+
+{
+	
+	student ob;
+	ob. accept();
+	ob.  disp();
+}
+
