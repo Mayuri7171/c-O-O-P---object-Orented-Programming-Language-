373f4c0dac2810229e119537a1447b345b2bb92f
Mayuri Balasaheb Lagad7171 <156415794+Mayuri7171@users.noreply.github.com>
2024-07-14 17:17:32 +0530

    Add files via upload

---
+++ MAXIMUM OF TWO NUMBERS.cpp
@@ -0,0 +1,23 @@
+// MAXIMUM OF TWO NUMBERS
+
+using namespace std;
+#include<conio.h>
+#include<iostream>
+int main()
+{
+	int a,b,c;
+	cout<<"enter the three number:";
+	 cin>>a>>b>>c;
+	 
+	 if(a>b&&a>c)
+	 
+	 cout<<"maximum no="<<a;
+	 
+	 else if (b>a&&b>c)
+	 
+	 cout<<"maximum no="<<b;
+	 
+	 else (c>a&&c>b);
+	 
+	 cout<<"maximum no="<<c;
+}
