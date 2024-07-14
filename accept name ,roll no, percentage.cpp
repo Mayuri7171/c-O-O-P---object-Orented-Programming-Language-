5b36a97b9f8fb6a6c2a5fc2ae3f04fc61d89b6c0
Mayuri Balasaheb Lagad7171 <156415794+Mayuri7171@users.noreply.github.com>
2024-07-14 17:31:46 +0530

    Add files via upload

---
+++ accept name ,roll no, percentage.cpp
@@ -0,0 +1,16 @@
+// accept name ,roll no, percentage
+
+using namespace std;
+#include<iostream>
+int main()
+{
+	int rno;
+	char name[50];
+	float per;
+	
+	cout<<"enter the roll no name per:";
+	
+	cin>>rno>>name>>per;
+	
+	cout<<"\n rno="<<rno<< "\n name ="<<name <<"\n per="<<per;
+}
