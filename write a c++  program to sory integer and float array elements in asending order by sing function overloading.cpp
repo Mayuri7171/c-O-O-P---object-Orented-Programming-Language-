db99212a9d4dee057c5044a41dbe3e1aa346dcff
Mayuri Balasaheb Lagad7171 <156415794+Mayuri7171@users.noreply.github.com>
2024-07-20 21:16:59 +0530

    Add files via upload

---
+++ write a c++  program to sory integer and float array elements in asending order by sing function overloading.cpp
@@ -0,0 +1,21 @@
+//write a c++  program to sory integer and float array elements in asending order by sing function overloading
+
+void sort(int a[],int n)
+{
+	
+}
+
+void sort(float a[], int  n)
+{
+	
+}
+
+
+int main()
+{
+	int a[]={ 11 , 33 , 22 , 55 , 66 , 44 , 66 ,99 };
+	float b[]={ 10.11 , 22.5 , 99.6  };
+	
+	sort(a,8);
+	sort(b,3);
+}
