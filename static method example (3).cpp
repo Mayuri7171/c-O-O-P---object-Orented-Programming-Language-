b1cae856b521e200a45443433d5c0be535933bd0
Mayuri Balasaheb Lagad7171 <156415794+Mayuri7171@users.noreply.github.com>
2024-07-20 21:43:20 +0530

    Add files via upload

---
+++ static method example (3).cpp
@@ -0,0 +1,33 @@
+
+// static method example (3)
+
+using namespace std;
+#include<iostream>
+
+  class mayu
+{
+	public:
+		
+		
+		static void add (int a ,int b)
+		
+		{
+			
+			int c=a+b;
+			
+			{
+			
+			
+			cout<<"\n addition="<<c<<endl;
+			
+		   } 
+		}
+} ob;
+
+
+             int main()
+             {
+             	mayu:: add (66,99);
+             	ob. add (36,69);
+			 }
+
